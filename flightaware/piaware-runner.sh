4582de567dd9bf6222533737dffdaf3fb75ed813
Guy Taylor <thebigguy.co.uk@gmail.com>
2018-07-10 21:39:59 +0100

    Start setting up flighaware

--- flightaware/piaware-runner.sh
+++ flightaware/piaware-runner.sh
@@ -10,4 +10,20 @@ set -o xtrace          # Trace the execution of the script (debug)
 echo "Waiting for dump1090 to start up"
 sleep 5s
 
+echo
+echo "FLIGHTAWARE_USERNAME=${FLIGHTAWARE_USERNAME}"
+echo "FLIGHTAWARE_PASSWORD=***"
+echo "FLIGHTAWARE_FEEDER_ID=${FLIGHTAWARE_FEEDER_ID}"
+echo
+
+
+piaware-config "flightaware-user" "${FLIGHTAWARE_USERNAME}"
+ piaware-config "flightaware-password" "${FLIGHTAWARE_PASSWORD}"
+
+if [ -z "${FLIGHTAWARE_FEEDER_ID}" ]; then
+    echo "No FLIGHTAWARE_FEEDER_ID set"
+else
+    piaware-config "feeder-id" "${FLIGHTAWARE_FEEDER_ID}"
+fi
+
 piaware -plainlog
