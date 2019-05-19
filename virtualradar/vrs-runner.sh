b11c2d80955c0fef0be6e63f4bcc74862ccb5969
Guy Taylor <thebigguy.co.uk@gmail.com>
2019-05-19 11:33:12 +0000

    Auto configure FlightAware in VirtualRadar

--- virtualradar/vrs-runner.sh
+++ virtualradar/vrs-runner.sh
@@ -21,12 +21,16 @@ DUMP1090_SERVER='dump1090'
 DUMP1090_PORT='30005'
 ADSBEXCHANGE_MLAT_SERVER='adsbexchange-mlat'
 ADSBEXCHANGE_MLAT_PORT='30104'
+FLIGHTAWARE_MLAT_SERVER='flightaware'
+FLIGHTAWARE_MLAT_PORT='30105'
 
 start_secion_spacer "Running with"
 echo " - DUMP1090_SERVER=${DUMP1090_SERVER}"
 echo " - DUMP1090_PORT=${DUMP1090_PORT}"
 echo " - ADSBEXCHANGE_MLAT_SERVER=${ADSBEXCHANGE_MLAT_SERVER}"
 echo " - ADSBEXCHANGE_MLAT_PORT=${ADSBEXCHANGE_MLAT_PORT}"
+echo " - FLIGHTAWARE_MLAT_SERVER=${FLIGHTAWARE_MLAT_SERVER}"
+echo " - FLIGHTAWARE_MLAT_PORT=${FLIGHTAWARE_MLAT_PORT}"
 echo " - VIRTUALRADAR_LATITUDE =${VIRTUALRADAR_LATITUDE}"
 echo " - VIRTUALRADAR_LONGITUDE=${VIRTUALRADAR_LONGITUDE}"
 end_secion_spacer
@@ -44,6 +48,7 @@ start_secion_spacer "Downloading route DB"
 curl --output '/tmp/StandingData.sqb.gz' 'http://www.virtualradarserver.co.uk/Files/StandingData.sqb.gz'
 zcat '/tmp/StandingData.sqb.gz' > '/tmp/StandingData.sqb'
 rm '/tmp/StandingData.sqb.gz'
+sqlite3 -readonly '/tmp/StandingData.sqb' 'SELECT COUNT(*) FROM Route'
 end_secion_spacer
 
 start_secion_spacer 'customising config'
@@ -51,6 +56,8 @@ sed -i "s/DUMP1090_SERVER/${DUMP1090_SERVER}/" /root/.local/share/VirtualRadar/C
 sed -i "s/DUMP1090_PORT/${DUMP1090_PORT}/" /root/.local/share/VirtualRadar/Configuration.xml
 sed -i "s/ADSBEXCHANGE_MLAT_SERVER/${ADSBEXCHANGE_MLAT_SERVER}/" /root/.local/share/VirtualRadar/Configuration.xml
 sed -i "s/ADSBEXCHANGE_MLAT_PORT/${ADSBEXCHANGE_MLAT_PORT}/" /root/.local/share/VirtualRadar/Configuration.xml
+sed -i "s/FLIGHTAWARE_MLAT_SERVER/${FLIGHTAWARE_MLAT_SERVER}/" /root/.local/share/VirtualRadar/Configuration.xml
+sed -i "s/FLIGHTAWARE_MLAT_PORT/${FLIGHTAWARE_MLAT_PORT}/" /root/.local/share/VirtualRadar/Configuration.xml
 sed -i "s/VIRTUALRADAR_LATITUDE/${VIRTUALRADAR_LATITUDE}/" /root/.local/share/VirtualRadar/Configuration.xml
 sed -i "s/VIRTUALRADAR_LONGITUDE/${VIRTUALRADAR_LONGITUDE}/" /root/.local/share/VirtualRadar/Configuration.xml
 end_secion_spacer
